713836580bd2df394e53e72adc8072cea062509b
Christian Enrique Matos Basaldúa <chbasaldua123@gmail.com>
2025-04-11 16:53:40 -0600

    Server Auth

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -2,7 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Unity.Netcode; // Namespace for Unity's Netcode for GameObjects
+using Unity.Netcode;
+using System.Security.Cryptography; // Namespace for Unity's Netcode for GameObjects
 
 // NetworkBehaviour is a class from the Mirror Networking library, which is used for multiplayer games in Unity.
 // It allows you to create networked objects that can synchronize their state across clients and the server.
@@ -61,7 +62,8 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
         if (!IsOwner) {
             return;
         }
-        HandleMovement();
+        HandleMovementServerAuth();
+        // HandleMovement();
         HandleInteractions();
     }
 
@@ -92,6 +94,56 @@ public class Player : NetworkBehaviour, IKitchenObjectParent {
         } else {
             SetSelectedCounter(null);
         }
+    }
+
+    private void HandleMovementServerAuth() {
+        Vector2 inputVector = GameInput.Instance.GetMovementVectorNormalized();
+        HandleMovementServerRpc(inputVector);
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void HandleMovementServerRpc(Vector2 inputVector) {
+        Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
+
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerRadius = .7f;
+        float playerHeight = 2f;
+        bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+        if (!canMove) {
+            // Cannot move towards moveDir
+
+            // Attempt only X movement
+            Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+            canMove = (moveDir.x < -.5f || moveDir.x > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+            if (canMove) {
+                // Can move only on the X
+                moveDir = moveDirX;
+            } else {
+                // Cannot move only on the X
+
+                // Attempt only Z movement
+                Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+                canMove = (moveDir.z < -.5f || moveDir.z > +.5f) && !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+                if (canMove) {
+                    // Can move only on the Z
+                    moveDir = moveDirZ;
+                } else {
+                    // Cannot move in any direction
+                }
+            }
+        }
+
+        if (canMove) {
+            transform.position += moveDir * moveDistance;
+        }
+
+        isWalking = moveDir != Vector3.zero;
+
+        float rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 
     private void HandleMovement() {
